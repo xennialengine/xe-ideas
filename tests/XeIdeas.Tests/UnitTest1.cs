cd6a15a765bd0c0594b847a090a2eb6aeba126f8
Karn Ratana <karn.ratana@gmail.com>
2020-04-21 20:43:25 -0400

    Add Unit tests (#2)
    Add a blank xUnit project.
    Add unit test step to CI.

---
+++ tests/XeIdeas.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace XeIdeas.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
