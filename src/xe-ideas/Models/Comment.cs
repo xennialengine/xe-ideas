d506e7773eae2bd7be7fb01f3fb2e47d1cb0e275
Karn Ratana <karn.ratana@gmail.com>
2020-07-03 13:20:39 -0400

    Add database tables and code models for - Ideas - Comments

---
+++ src/xe-ideas/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace xe_ideas.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string CreatorId { get; set; }
+        public ApplicationUser Creator { get; set; }
+
+        public int IdeaId { get; set; }
+        public Idea Idea { get; set; }
+
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime LastModifiedDate { get; set; } = DateTime.UtcNow;
+    }
+}
