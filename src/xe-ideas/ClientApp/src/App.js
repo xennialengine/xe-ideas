984200cf0b931d49016b72746cf391c8e07763ed
Karn Ratana <karn@ratana.info>
2020-12-28 15:39:32 +0000

    Update App.js

--- src/xe-ideas/ClientApp/src/App.js
+++ src/xe-ideas/ClientApp/src/App.js
@@ -4,6 +4,9 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { IdeaList } from './components/idea/IdeaList';
+import { IdeaDetails } from './components/idea/IdeaDetails';
+import { IdeaDetailsEdit } from './components/idea/IdeaDetailsEdit';
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute';
 import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes';
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
@@ -19,6 +22,10 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <AuthorizeRoute path='/fetch-data' component={FetchData} />
+        <AuthorizeRoute path='/ideas' exact component={IdeaList}  />
+        <AuthorizeRoute path='/u/:username/ideas' exact component={IdeaList}  />
+        <AuthorizeRoute path='/i/:ideaId' exact component={IdeaDetails}  />
+        <AuthorizeRoute path='/i/:ideaId/edit' exact component={IdeaDetailsEdit}  />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
       </Layout>
     );
